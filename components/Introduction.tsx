0db0c6a4621502b4b5beb31c43f97cc08de642d4
Shiyu Wu <swuu0040@student.monash.edu>
2023-08-31 13:17:44 +1000

    delete team info

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -20,74 +20,7 @@ export default function Introduction() {
           The mission of our team is to convert the invisible asthma into tangible data and information to communicate to you,
           and to better spread knowledge about asthma to children to help them and their parents live better lives.
       </Text>
-        <Flex align="center" justify="center" wrap="wrap"  gap='3'>
-          <Box textAlign={'center'}>
-            <Avatar
-              src={
-                'https://i.imgur.com/dOH48NE.jpg'
-              }
-              mb={2}
-            />
-
-            <Text fontWeight={600}>Ridong Jiang</Text>
-            <Text fontSize={'sm'} color={useColorModeValue('gray.400', 'gray.400')}>
-                Full Stack developer
-            </Text>
-          </Box>
-            <Box textAlign={'center'}>
-                <Avatar
-                    src={
-                        'https://i.imgur.com/QeUEUK9.png'
-                    }
-                    mb={2}
-                />
-
-                <Text fontWeight={600}>Shiyu Wu</Text>
-                <Text fontSize={'sm'} color={useColorModeValue('gray.400', 'gray.400')}>
-                    Frontend developer
-                </Text>
-            </Box>
-            <Box textAlign={'center'}>
-                <Avatar
-                    src={
-                        'https://i.imgur.com/QeUEUK9.png'
-                    }
-                    mb={2}
-                />
-
-                <Text fontWeight={600}>Kaijia Yu</Text>
-                <Text fontSize={'sm'} color={useColorModeValue('gray.400', 'gray.400')}>
-                    Project Manager
-                </Text>
-            </Box>
-            <Box textAlign={'center'}>
-                <Avatar
-                    src={
-                        'https://i.imgur.com/QeUEUK9.png'
-                    }
-                    mb={2}
-                />
-
-                <Text fontWeight={600}>Sonia Lakhani</Text>
-                <Text fontSize={'sm'} color={useColorModeValue('gray.400', 'gray.400')}>
-                    Cyber Security
-                </Text>
-            </Box>
-            <Box textAlign={'center'}>
-                <Avatar
-                    src={
-                        'https://i.imgur.com/QeUEUK9.png'
-                    }
-                    mb={2}
-                />
-
-                <Text fontWeight={600}>Navrattan Singh Dhillon</Text>
-                <Text fontSize={'sm'} color={useColorModeValue('gray.400', 'gray.400')}>
-                    Data Engineer
-                </Text>
-            </Box>
-
-        </Flex>
+        
     </Stack>
   )
 }
