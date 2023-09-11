8abfcaf8b003734164d3213762a42d95746f5d03
Ridong Jiang <cianjiang597@gmail.com>
2023-09-12 00:11:20 +1000

    11

--- app/Trigger/page.tsx
+++ app/Trigger/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import TriggerHeroSection from './hero'
+// @ts-ignore
+//import Upload from './image'
 
 export default function Trigger() {
   return (
-    <TriggerHeroSection/>
-
+      <><TriggerHeroSection/>
+        {/*<Upload/>*/}
+      </>
   )
 }
---
+++ pages/plant.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+
+export default function Upload() {
+    const [image, setImage] = useState(null);
+    const [type, setType] = useState('Plant');
+    const [result, setResult] = useState(null);
+    const api_key = 'i23a3ushd123egiqwuoasqw2378qer12132';
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        setImage(file);
+    };
+
+    const handleTypeChange = (e) => {
+        setType(e.target.value);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('image', image);
+        formData.append('type', type);
+        formData.append('api_key', api_key);
+
+        console.log(formData);
+        const response = await fetch('http://localhost:5000/image_recognition', {
+            method: 'POST',
+            headers: {
+                'api_key': api_key, // Replace with your actual API key
+            },
+            body: formData,
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            setResult(data);
+        } else {
+            setResult(`Error: ${response.status}`);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Image Recognition</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="type">Type:</label>
+                    <select id="type" value={type} onChange={handleTypeChange}>
+                        <option value="Plant">Plant</option>
+                        <option value="Cat">Cat</option>
+                        <option value="Dog">Dog</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="image">Image:</label>
+                    <input type="file" id="image" accept="image/*" onChange={handleImageChange} />
+                </div>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+            {result && (
+                <div>
+                    <h2>Result:</h2>
+                    <pre>{JSON.stringify(result, null, 2)}</pre>
+                </div>
+            )}
+        </div>
+    );
+}
