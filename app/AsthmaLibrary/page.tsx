e204d8f389b6757e072ca349fcad02823e1f0e63
Shiyu Wu <swuu0040@student.monash.edu>
2023-09-21 19:22:10 +1000

    add footer

--- app/AsthmaLibrary/page.tsx
+++ app/AsthmaLibrary/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Sidebar from './Sidebar'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
     <>
     <Sidebar/>
+    <Footer/>
     </>
   )
 }
--- app/TriggerLibrary/page.tsx
+++ app/TriggerLibrary/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Cards from './cards'
+import Footer from '@/components/Footer'
 
 export default function page() {
   return (
+    <>
     <Cards/>
+    <Footer/>
+    </>
   )
 }
