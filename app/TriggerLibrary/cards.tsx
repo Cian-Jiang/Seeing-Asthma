78f051e4b44cdee209e04a7af8e56dbae9cdb7a7
Shiyu Wu <swuu0040@student.monash.edu>
2023-10-02 20:47:55 +1100

    update card flipper change

--- app/TriggerLibrary/cards.tsx
+++ app/TriggerLibrary/cards.tsx
@@ -1,85 +1,106 @@
 'use client'
 import { useEffect, useState } from 'react';
-import { Heading,Text, useDisclosure, Box, Tabs,  Text as ChakraText , TabList, Tab, TabPanels, TabPanel, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Flex, useColorModeValue, Divider, Center } from "@chakra-ui/react";
+import { Heading, Text, Box, Tabs, Text as ChakraText, TabList, Tab, TabPanels, TabPanel, Button, Flex, useColorModeValue, Divider, Center } from "@chakra-ui/react";
 
 export default function Cards() {
     const [data, setData] = useState({ cats: [], dogs: [], objects: [], plants: [] });
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const [openModalId, setOpenModalId] = useState<number | null>(null);
+    const [isFlipped, setIsFlipped] = useState<number | null>(null);
+    const [supportsHover, setSupportsHover] = useState(false);
 
     useEffect(() => {
         fetch('/api/getTriggers')
             .then((res) => res.json())
             .then((newData) => setData(newData));
 
+        // Check if device supports hover
+        if (window.matchMedia('(hover: hover)').matches) {
+            setSupportsHover(true);
+        }
     }, []);
-    // console.log(data);
 
-    interface Item {
-        id: number;
-        name: string;
-        objdes: string;
-        imageurl: string;
-    }
+    const handleHoverEnter = (id: number) => {
+        if (supportsHover) {
+            setIsFlipped(id);
+        }
+    };
+    
+    const handleHoverLeave = () => {
+        if (supportsHover) {
+            setIsFlipped(null);
+        }
+    };
+    
+    const handleClick = (id: number) => {
+        if (!supportsHover) {
+            setIsFlipped(prev => prev !== id ? id : null);
+        }
+    };
 
-    const renderCards = (items: Item[]) => {
-
+    const renderCards = (items: any[]) => {
         return (
             <Flex wrap="wrap" justify="space-between">
                 {items.map((item) => (
                     <Box
-
                         key={item.id}
-                        flex="0 0 calc(33% - 1rem)"
+                        flex={['0 0 calc(100% - 1rem)', '0 0 calc(50% - 1rem)', '0 0 calc(33.33% - 1rem)']}
                         height="400px"
                         borderWidth="1px"
                         borderRadius="md"
                         overflow="hidden"
                         mb="2rem"
+                        onMouseEnter={() => handleHoverEnter(item.id)}
+                        onMouseLeave={handleHoverLeave}
+                        onClick={() => handleClick(item.id)}
+                        position="relative"
+                        style={{
+                            transition: 'transform 0.6s',
+                            transformStyle: 'preserve-3d'
+                        }}
                     >
-                        <Box height="300px" overflow="hidden">
-                            <img
-                                src={item.imageurl}
-                                alt={item.name}
-                                onClick={() => { setOpenModalId(item.id); onOpen(); }}
-                                style={{
-                                    width: '100%',
-                                    height: '100%',
-                                    objectFit: 'cover'
-                                }}
-                            />
+                        {/* front of the card */}
+                        <Box position="absolute" width="100%" height="100%">
+                            <Box height="300px" overflow="hidden">
+                                <img
+                                    src={item.imageurl}
+                                    alt={item.name}
+                                    style={{
+                                        width: '100%',
+                                        height: '100%',
+                                        objectFit: 'cover'
+                                    }}
+                                />
+                            </Box>
+                            <Box p="4" flexDirection="column" alignItems="center">
+                                <ChakraText fontWeight="bold" textAlign="center" mb="2">
+                                    {item.name}
+                                </ChakraText>
+                            </Box>
                         </Box>
-                        <Box p="4" flexDirection="column" alignItems="center">
-                            <ChakraText fontWeight="bold" textAlign="center" mb="2">
-                                {item.name}
-                            </ChakraText>
-                            <Button onClick={() => { setOpenModalId(item.id); onOpen(); }}>More info</Button>
-                            <Modal isOpen={isOpen && openModalId === item.id} onClose={() => { setOpenModalId(null); onClose(); }}>
-
-                                <ModalOverlay />
-                                <ModalContent>
-                                    <ModalHeader>{item.name}</ModalHeader>
-                                    <ModalCloseButton />
-                                    <ModalBody>{item.objdes}</ModalBody>
-                                    <ModalFooter>
-                                        <Button colorScheme="blue" mr={3} onClick={onClose}>
-                                            Close
-                                        </Button>
-                                    </ModalFooter>
-                                </ModalContent>
-                            </Modal>
+    
+                        {/* back of the card */}
+                        <Box position="absolute" width="100%" height="100%" backgroundColor="white" display={isFlipped === item.id ? 'block' : 'none'} >
+                            <Box p="4" flexDirection="column" alignItems="center">
+                                <ChakraText fontWeight="bold" textAlign="center" mb="2">
+                                    {item.name}
+                                </ChakraText>
+                                <br/>
+                                <ChakraText fontWeight="normal" textAlign="center" mb="2">
+                                    {item.objdes}
+                                </ChakraText>
+                            </Box>
                         </Box>
                     </Box>
                 ))}
             </Flex>
         );
     };
+    
     const colors = useColorModeValue(
-        ['red.50', 'red.50',  'green.50',  'green.50'],
-        ['red.900', 'red.900', 'green.900', 'green.900'],
-    )
-    const [tabIndex, setTabIndex] = useState(0)
-    const bg = colors[tabIndex]
+        ['red.50', 'red.50', 'green.50', 'green.50'],
+        ['red.900', 'red.900', 'green.900', 'green.900']
+    );
+    const [tabIndex, setTabIndex] = useState(0);
+    const bg = colors[tabIndex];
 
     return (
 
@@ -98,7 +119,7 @@ export default function Cards() {
                 <br/>
                 <Divider />
                 <br/>
-                <TabList justifyContent="center" >
+                <TabList justifyContent="center" flexWrap="wrap">
                     <Tab>Common ASTHMA Triggers</Tab>
                     <Tab>Asthma-inducing plants</Tab>
                     <Center height='50px'>
